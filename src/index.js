499c72732c0d5b82afee7b5cdff73f8b28dad93d
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-02-21 00:03:10 +0300

    feat: add task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) { 
+    return +String(n).split('').filter(e => {return (e !== '-')}).reverse().join('');
+  }
+  return +String(n).split('').reverse().join('');
 }
